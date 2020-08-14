5b490783b8c524ac70e972a4f117495b2bc87046
SEULGI YOON <sgyoon.info@gmail.com>
2020-08-14 21:33:47 +0900

    2008014 Chapter 4.1.8

--- chapter-4/src/index.ts
+++ chapter-4/src/index.ts
@@ -18,3 +18,20 @@ function formDate(this: Date) {
 
 // formDate(); // The 'this' context of type 'void' is not assignable to method's 'this' of type 'Date'.
 formDate.call(new Date());
+
+// p.71 문맥적 타입화
+function times(f: (index: number) => void, n: number) {
+  for (let i = 0; i < n; i++) {
+    f(i);
+  }
+}
+times((n) => console.log(`${n}!`), 4);
+// 인라인으로 함수를 제공하면 전달하는 함수의 타입을 명시할 필요가 없다.
+
+// function callback(n) {
+//   console.log(`${n}!`);
+// };
+// Error: Parameter 'n' implicitly has an 'any' type.
+
+// times(callback, 4);
+// 인라인으로 제공하지 않을 시 타입스크립트는 타입을 추론할 수 없다.
